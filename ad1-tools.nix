0f00d69023a82a6075cfbc40eb6ade3273af8043
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-09-18 07:15:55 +0800

    Create ad1-tools.nix

---
+++ ad1-tools.nix
@@ -0,0 +1,28 @@
+{ lib, stdenv, bash, autoreconfHook, pkg-config, which
+, fetchFromGitLab
+}: 
+let
+  version = "1.0";
+  src = fetchFromGitLab {
+    owner = "al3ks1s";
+    repo = "AD1-tools";
+    rev = "v${version}";
+    hash = lib.fakeHash;
+  };
+in
+stdenv.mkDerivation {
+  pname = "ad1-tools";
+  inherit version src;
+
+  enableParallelBuilding = true;
+
+  nativeBuildInputs = [
+  ]
+  buildInputs = [
+  ]
+
+  preConfigure = ''
+    patchShebangs --build ./autogen.sh
+    ./autogen.sh
+  '';
+}
